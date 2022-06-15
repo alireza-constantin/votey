dce5d2ec897f713b539c4b87801f31e90febf614
Alireza Soheili <71215827+alireza-constantin@users.noreply.github.com>
2022-06-15 16:05:33 +0430

    sqlite and prisma up and running

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,22 @@
-import type { NextPage } from 'next';
+import { prisma } from '@/db/client';
+import type { NextPage, GetServerSideProps } from 'next';
 
-const Home: NextPage = () => {
-	return <div className="text-3xl text-red-600">Hello</div>;
+const Home: NextPage = (props: any) => {
+	return (
+		<div className="text-3xl text-red-600">
+			<code>{props.questions}</code>
+		</div>
+	);
 };
 
 export default Home;
+
+export const getServerSideProps: GetServerSideProps = async () => {
+	const pollQuestions = await prisma.question.findMany();
+
+	return {
+		props: {
+			questions: JSON.stringify(pollQuestions),
+		},
+	};
+};
