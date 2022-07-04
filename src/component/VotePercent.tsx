5f6a04bfc3663c6bd6af6ab2acd62f11cdb8e8f9
Alireza Soheili <71215827+alireza-constantin@users.noreply.github.com>
2022-07-04 13:29:33 +0430

    add show vote percent

---
+++ src/component/VotePercent.tsx
@@ -0,0 +1,46 @@
+import type { FC } from 'react';
+
+const VotePercent: FC<{
+	choice: number | undefined;
+	count: number;
+	text: string;
+	index: number;
+	totalVotes: number;
+}> = ({ choice, count, text, index, totalVotes }) => {
+	function calcPercent(totalVotes: number, count: number): number {
+		if (totalVotes === 0 || count === 0) return 0;
+		return (count / totalVotes) * 100;
+	}
+
+	console.log(calcPercent(totalVotes, count));
+
+	return (
+		<div className="mb-4 md:w-2/3 mx-auto">
+			<div className="flex justify-between mb-2">
+				<span className="text-base flex gap-1 items-center font-medium text-gray-400 capitalize">
+					<span>
+						{choice === index && (
+							<svg
+								xmlns="http://www.w3.org/2000/svg"
+								className="h-6 w-6"
+								fill="none"
+								viewBox="0 0 24 24"
+								stroke="currentColor"
+								strokeWidth={2}
+							>
+								<path strokeLinecap="round" strokeLinejoin="round" d="M5 13l4 4L19 7" />
+							</svg>
+						)}
+					</span>{' '}
+					{text}
+				</span>
+				<span className="text-sm font-medium text-gray-400">{calcPercent(totalVotes, count).toFixed(2)}%</span>
+			</div>
+			<div className="w-full bg-slate-700 rounded-lg h-5">
+				<div className="bg-fuchsia-500/70 h-5 rounded-lg" style={{ width: `${calcPercent(totalVotes, count)}%` }}></div>
+			</div>
+		</div>
+	);
+};
+
+export default VotePercent;
--- src/pages/question/[id].tsx
+++ src/pages/question/[id].tsx
@@ -3,35 +3,51 @@ import { useRouter } from 'next/router';
 import React from 'react';
 import type { createQuestionType } from '@/utils/validator';
 import Vote from '@/component/Vote';
+import VotePercent from '@/component/VotePercent';
 
 const QuestionContent: React.FC<{ id: string }> = ({ id }) => {
 	const { data, isLoading } = trpc.useQuery(['questions.getById', { id }]);
 
+	let totalVotes = 0;
 	// voting mutation
 	const { mutate, data: voteResponse } = trpc.useMutation(['questions.vote'], {
-		onSuccess: () => window.location.reload(),
+		onSuccess: () => {
+			window.location.reload();
+		},
 	});
 
 	if (!isLoading && !data) {
 		return <div>Question Not Found</div>;
 	}
 
+	const getTotalVotes = (votes: any) => {
+		votes?.map((choice: { _count: number }) => {
+			totalVotes += choice._count;
+		});
+	};
+
 	const voteHandler = (questionId: string, idx: number) => {
 		mutate({ questionId, option: idx });
 	};
+
+	if (data && data != undefined) getTotalVotes(data.votes);
 
 	console.log(data);
 
 	return (
 		<div>
-			{data?.isOwner && <p>You are owner</p>}
 			<h2 className="text-center my-4 font-semibold text-2xl capitalize text-gray-300">{data?.question?.question}</h2>
 			{(data?.question?.options as string[])?.map((option, idx) => {
 				if (data?.isOwner || data?.vote) {
 					return (
-						<div key={idx} className={data.vote?.choice === idx ? 'underline' : ''}>
-							{data?.votes?.[idx]?._count ?? 0} - {(option as any).text}
-						</div>
+						<VotePercent
+							totalVotes={totalVotes}
+							key={idx}
+							index={idx}
+							choice={data.vote?.choice}
+							text={(option as any).text}
+							count={data?.votes?.[idx]?._count ?? 0}
+						/>
 					);
 				}
 
@@ -41,6 +57,25 @@ const QuestionContent: React.FC<{ id: string }> = ({ id }) => {
 					</div>
 				);
 			})}
+			{data?.isOwner && (
+				<p className="mt-10 flex justify-center gap-2 font-bold text-gray-300 items-center rounded mx-auto p-3 text-center sm:w-1/4 bg-slate-50/10">
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						className="h-6 w-6"
+						fill="none"
+						viewBox="0 0 24 24"
+						stroke="currentColor"
+						strokeWidth={2}
+					>
+						<path
+							strokeLinecap="round"
+							strokeLinejoin="round"
+							d="M11.049 2.927c.3-.921 1.603-.921 1.902 0l1.519 4.674a1 1 0 00.95.69h4.915c.969 0 1.371 1.24.588 1.81l-3.976 2.888a1 1 0 00-.363 1.118l1.518 4.674c.3.922-.755 1.688-1.538 1.118l-3.976-2.888a1 1 0 00-1.176 0l-3.976 2.888c-.783.57-1.838-.197-1.538-1.118l1.518-4.674a1 1 0 00-.363-1.118l-3.976-2.888c-.784-.57-.38-1.81.588-1.81h4.914a1 1 0 00.951-.69l1.519-4.674z"
+						/>
+					</svg>
+					You are owner
+				</p>
+			)}
 		</div>
 	);
 };
