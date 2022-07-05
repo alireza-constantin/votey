226eb00cc14c32570667b64ae64d38873d629b5f
Alireza Soheili <71215827+alireza-constantin@users.noreply.github.com>
2022-07-05 15:43:14 +0430

    clean the question page

--- src/pages/question/[id].tsx
+++ src/pages/question/[id].tsx
@@ -5,6 +5,8 @@ import Vote from '@/component/Vote';
 import VotePercent from '@/component/VotePercent';
 import Loader from '@/component/Loader';
 import { formatDistance } from 'date-fns';
+import OwnerBadge from '@/component/OwnerBadge';
+import { Prisma } from '@prisma/client';
 
 const QuestionContent: React.FC<{ id: string }> = ({ id }) => {
 	const { data, isLoading } = trpc.useQuery(['questions.getById', { id }]);
@@ -43,14 +45,15 @@ const QuestionContent: React.FC<{ id: string }> = ({ id }) => {
 
 	return (
 		<div className="mt-16">
-			<div className="flex justify-between items-center md:w-2/3 mx-auto mb-6">
-				<h2 className="text-center my-4 font-semibold text-2xl capitalize text-gray-300">{data?.question?.question}</h2>
-				<div className="text-sm text-gray-400">
-					{data?.isEnded ? 'Finished ' : 'Ends '}{' '}
-					{data?.question?.endsAt && formatDistance(data.question.endsAt, new Date(), { addSuffix: true })}
-					{!data?.question?.endsAt && 'in infinite time'}
-				</div>
-			</div>
+			<CardHeader
+				question={data?.question?.question!}
+				text={data?.isEnded ? 'Finished ' : 'Ends '}
+				date={
+					data?.question?.endsAt
+						? formatDistance(data.question.endsAt, new Date(), { addSuffix: true })
+						: 'in infinite time'
+				}
+			/>
 			{(data?.question?.options as string[])?.map((option, idx) => {
 				if (data?.isOwner || data?.vote || data?.isEnded) {
 					return (
@@ -77,25 +80,19 @@ const QuestionContent: React.FC<{ id: string }> = ({ id }) => {
 					</div>
 				);
 			})}
-			{data?.isOwner && (
-				<p className="mt-10 flex justify-center gap-2 font-bold text-gray-300 items-center rounded mx-auto p-3 text-center sm:w-1/4 bg-slate-50/10">
-					<svg
-						xmlns="http://www.w3.org/2000/svg"
-						className="h-6 w-6"
-						fill="none"
-						viewBox="0 0 24 24"
-						stroke="currentColor"
-						strokeWidth={2}
-					>
-						<path
-							strokeLinecap="round"
-							strokeLinejoin="round"
-							d="M11.049 2.927c.3-.921 1.603-.921 1.902 0l1.519 4.674a1 1 0 00.95.69h4.915c.969 0 1.371 1.24.588 1.81l-3.976 2.888a1 1 0 00-.363 1.118l1.518 4.674c.3.922-.755 1.688-1.538 1.118l-3.976-2.888a1 1 0 00-1.176 0l-3.976 2.888c-.783.57-1.838-.197-1.538-1.118l1.518-4.674a1 1 0 00-.363-1.118l-3.976-2.888c-.784-.57-.38-1.81.588-1.81h4.914a1 1 0 00.951-.69l1.519-4.674z"
-						/>
-					</svg>
-					You are owner
-				</p>
-			)}
+			{data?.isOwner && <OwnerBadge />}
+		</div>
+	);
+};
+
+const CardHeader = ({ question, text, date }: { question: string; text: string; date: string }) => {
+	return (
+		<div className="flex justify-between items-center md:w-2/3 mx-auto mb-6">
+			<h2 className="text-center my-4 font-semibold text-2xl capitalize text-gray-300">{question}</h2>
+			<div className="text-sm text-gray-400">
+				<span>{text}</span>
+				<span>{date}</span>
+			</div>
 		</div>
 	);
 };
