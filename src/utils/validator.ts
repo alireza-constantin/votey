ca838173d3955f228720cadb3fb90c6e067a3829
Alireza Soheili <71215827+alireza-constantin@users.noreply.github.com>
2022-08-11 21:42:14 +0430

    better form error message

--- src/utils/validator.ts
+++ src/utils/validator.ts
@@ -2,7 +2,7 @@ import { z } from 'zod';
 
 
 export const createQuestionValidator = z.object({
-    question: z.string().min(5).max(600),
+    question: z.string().min(5, { message: 'Must be 5 or more characters long' }).max(500, { message: 'Must be 500 or fewer characters long' }),
     options: z.array(z.object({ text: z.string().min(2).max(30) })).min(2).max(10),
     endsAt: z.string().or(z.date()).or(z.null())
 });
