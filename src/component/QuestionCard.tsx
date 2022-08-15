e5c525a32a637a5faaf708d379f1fb90dac7d3af
Alireza Soheili <71215827+alireza-constantin@users.noreply.github.com>
2022-08-15 12:33:13 +0430

    fix the height of the cards

--- src/component/QuestionCard.tsx
+++ src/component/QuestionCard.tsx
@@ -13,16 +13,18 @@ const QuestionCard: React.FC<{
 			initial={{ opacity: 0, scale: 0.7 }}
 			animate={{ opacity: 1, scale: 1 }}
 			transition={{ duration: 0.4, ease: [0.77, 0.67, 0.43, 0.37] }}
+			className="h-full"
 		>
 			<Link href={`/question/${question.id}`}>
 				<div
 					key={question.id}
-					className="flex flex-col justify-between cursor-pointer rounded-md px-4 pb-2 pt-4 bg-indigo-900/20 shadow-xl hover:shadow-2xl  shadow-black/50"
+					className="flex flex-col justify-between cursor-pointer rounded-md px-4 pb-2 pt-4 bg-indigo-900/20 shadow-xl hover:shadow-2xl  shadow-black/50 h-full"
 				>
 					<div>
 						<h1 className="text-lg break-words font-bold capitalize" key={question.id}>
 							{question.question}
 						</h1>
+
 						<p className="text-xs flex gap-1 items-center mt-4 text-white/30">
 							<span>
 								<svg
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -32,9 +32,9 @@ const Home: NextPage = () => {
 			<Head>
 				<title>Home | Votey</title>
 			</Head>
-
-			<div className="grid grid-cols-1 gap-y-5 md:grid-cols-3 lg:grid-cols-4 md:gap-x-5 mt-10">
-				{data?.map((question, idx) => (
+			{/* grid-cols-1  md:grid-cols-3 lg:grid-cols-4 md:gap-x-5 */}
+			<div className="grid gap-y-5 grid-cols-1  md:grid-cols-3 lg:grid-cols-4 md:gap-x-5 mt-10">
+				{data?.map((question) => (
 					<QuestionCard key={question.id} question={question} copyToClipboard={copyToClipboard} />
 				))}
 			</div>
